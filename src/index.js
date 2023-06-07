4e232744dbf875faf57435b450531c6195ca1c0c
mansidhamelia <mansidhamelia6@gmail.com>
2023-06-06 17:34:29 -0700

    toggle favorite using context api

--- src/index.js
+++ src/index.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
-import productReducer from './store/reducers/products';
+import ProductProvider from './context/Products-context';
 
-const rootReducer = combineReducers({
-  shop: productReducer
-});
-
-const store = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
+  <ProductProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>);
+  </ProductProvider>);
