c671738ba0f478e1296905ae80bccc584c55d6c4
mansidhamelia <mansidhamelia6@gmail.com>
2023-06-09 14:49:37 -0700

    writing first test

---
+++ src/components/Greeting.test.js
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react"
+import Greeting from "./Greeting"
+
+
+test('renders hello world as a text', () => {
+    // Arrange
+    render(<Greeting />)
+
+    // Act
+    // ...nothing
+
+    // Assert
+    const helloElement = screen.getByText('Hello Greetings', { exact: false })  //exact: false will check only words but not precisely
+    expect(helloElement).toBeInTheDocument();
+
+
+})
