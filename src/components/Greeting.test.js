7ed2b8855bb89885cafe81799006ded75d15cb3b
mansidhamelia <mansidhamelia6@gmail.com>
2023-06-09 14:54:06 -0700

    grouping test with tests suites

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,17 +1,18 @@
 import { render, screen } from "@testing-library/react"
 import Greeting from "./Greeting"
 
+describe('Greeting component', () => {
+    test('renders hello world as a text', () => {
+        // Arrange
+        render(<Greeting />)
 
-test('renders hello world as a text', () => {
-    // Arrange
-    render(<Greeting />)
+        // Act
+        // ...nothing
 
-    // Act
-    // ...nothing
-
-    // Assert
-    const helloElement = screen.getByText('Hello Greetings', { exact: false })  //exact: false will check only words but not precisely
-    expect(helloElement).toBeInTheDocument();
+        // Assert
+        const helloElement = screen.getByText('Hello Greetings', { exact: false })  //exact: false will check only words but not precisely
+        expect(helloElement).toBeInTheDocument();
 
 
+    })
 })
