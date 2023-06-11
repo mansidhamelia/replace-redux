db83cbfbbfdef01de6f3472b8932310c22e67b59
mansidhamelia <mansidhamelia6@gmail.com>
2023-06-11 15:31:46 -0700

    type aliases

--- basics.ts
+++ basics.ts
@@ -16,12 +16,15 @@ let abc: null;  //can not assign any type of value
 let hobbies: string[];
 hobbies = ['sports', 'Cooking'];
 
-let xyz: any;  //anything can allowed such as object
-
-let person: {
+// type aliases
+type Person = {
     name: string,
     age: number
 }
+
+let xyz: any;  //anything can allowed such as object
+
+let person: Person
 
 person = {
     name: 'Mansi',
@@ -35,14 +38,13 @@ person = {
 //     isEmployee: true
 // }
 
-let people: {
-    name: string,
-    age: number
-}[];
+let people: Person[];
 
 // Type inference
 
-let course = 'React - typescript guide';
-// course = 123456  //cannot assign
+let course: string | number = 'React - typescript guide';
+course = 12345
+
+
 
 
