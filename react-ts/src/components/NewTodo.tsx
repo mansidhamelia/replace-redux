6d6f6a14be972b5d46548d0cfe6ae908e238792e
mansidhamelia <mansidhamelia6@gmail.com>
2023-06-12 13:19:01 -0700

    working with function props

--- react-ts/src/components/NewTodo.tsx
+++ react-ts/src/components/NewTodo.tsx
@@ -1,6 +1,6 @@
 import React, { useRef } from "react";
 
-const NewTodo = () => {
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
     const todoTextRef = useRef<HTMLInputElement>(null)
 
     const submitHandler = (event: React.FormEvent) => {
@@ -11,6 +11,8 @@ const NewTodo = () => {
             // throw an error
             return;
         }
+
+        props.onAddTodo(enteredText)
 
     }
 
